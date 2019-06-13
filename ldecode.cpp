0cfa60e133afbef2f38b0a928605781efce4165d
YAOYELU <luyaoyejeren@gmail.com>
2019-06-13 20:19:41 +1000

    done with ldecode, works fine

--- ldecode.cpp
+++ ldecode.cpp
@@ -42,14 +42,12 @@ int main(int argc, char* argv[]){
     //after the last character, there will no more delimiter before "\n"
     //so need to add the last code word.
     codewords.push_back(elemt);
-    // for(vector<string>::iterator it = codewords.begin();it != codewords.end();++it){
-    //     cout<<*it<<' ';
-    // }
+    
     vector<string> cmd;
     for(int i = 0;i<argc;i++){//c-style
         cmd.push_back(argv[i]);
     }
-    if(find(cmd.begin(),cmd.end(),"-l") != cmd.end()){
+    if(find(cmd.begin(),cmd.end(),"-l") != cmd.end()){//command line with "-l"
         for(vector<string>::iterator it = codewords.begin();it!=codewords.end();++it){
             if(it == codewords.begin()){
                 cout<<"NIL"<<' '<<*it<<' '<<*it<<'\n';
@@ -108,6 +106,61 @@ int main(int argc, char* argv[]){
                 w = k;
             }
         }
+    }else{//command line without "-l"
+        for(vector<string>::iterator it = codewords.begin();it!=codewords.end();++it){
+            if(it == codewords.begin()){
+                cout<<*it;
+                //next w 
+                w = *it;
+            }else{
+                string k = *it;
+                string output;
+                string symbol;
+                bool is_catch = false;
+                //print output
+                //consider index larger than the biggest one in the dictionary
+                if(k.size()>1){// k is a dictionary reference
+                    if(lzwDict.find(k) != lzwDict.end()){
+                    // k is in the dictionary
+                        output = lzwDict[k];
+                    }else{// k is not in the dictionary
+                        is_catch = true;
+                        if(lzwDict.find(w) != lzwDict.end()){
+                            // w is a dictionary reference
+                            output = lzwDict[w]+lzwDict[w][0];
+                        }else{
+                            // w is a character
+                            output = w+w;
+                        }
+                    }
+                }else{//k is a character
+                    output = k;
+                }
+                cout<<output;
+                if(lzwDict.size() <= 16384 - 256){
+                    if(is_catch){
+                        symbol = output;
+                    }else{
+                        if(w.size()>1){//w is a dictionary reference
+                            if(k.size()>1){ //k is a dictionary reference
+                                symbol = (lzwDict[w]+ lzwDict[k][0]);
+                            }else{//k is a character
+                                symbol = (lzwDict[w]+ k);
+                            }
+                        }else{//w is a character
+                            if(k.size()>1){ //k is a dictionary reference
+                                symbol = (w+ lzwDict[k][0]);
+                            }else{//k is a character
+                                symbol = (w+ k);
+                            }
+                        }
+                    }
+                    lzwDict.insert(pair<string,string>(to_string(index++),symbol));
+                }
+                w = k;
+            }
+        }
+        cout<<'\n';
     }
     return 0;
 }
